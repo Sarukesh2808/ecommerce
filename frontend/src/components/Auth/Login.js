3a4fe37e95c70c6d93569f5d64752b18d10b7e06
Sarukesh S <96428657+Sarukesh2808@users.noreply.github.com>
2024-10-10 22:27:06 +0530

    Update Login.js

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/users/login', { email, password });
+            const response = await axios.post('https://ecommerce-backend-saw5.onrender.com/api/users/login', { email, password });
             localStorage.setItem('token', response.data.token);
             alert('Login successful!');
             navigate('/');
